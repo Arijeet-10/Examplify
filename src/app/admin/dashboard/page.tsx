cf175e884900cbbef0772dd00ebddf981bec0c43
Sayan Kr. Rakshit <sayan12842@gmail.com>
2025-08-28 07:53:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -8,6 +8,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -266,4 +267,3 @@ export default function AdminDashboard() {
     </div>
   );
 }
-
